ff8d42f82cfaf3e0ea4ab49f56592c2d530cc167
budhaditya-itobuz <it012303@IT012303s-MacBook-Pro.local>
2024-03-12 14:49:28 +0530

    wip

--- frontend/index.js
+++ frontend/index.js
@@ -29,26 +29,30 @@ form.addEventListener("submit", (e) => {
     weather(dataObject.location);
 });
 
+
+
 const weather = async (location) => {
     theme.classList.remove("day", "night", "rain");
     try {
         showError.innerText=""
         container.classList.remove('d-none')
         const res = await fetch(
-            `https://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${location}&aqi=no`
+            `http://localhost:4000/${location}`
         );
-        const data = await res.json();
-        const date = new Date(data.location.localtime);
+        const response = await res.json();
+        const {data}=response
+        console.log(data)
+        const date = new Date(data.localtime);
 
-        if (data.current.precip_mm > 0) theme.classList.add("rain");
+        if (data.precip_mm > 0) theme.classList.add("rain");
         else if (date.getHours() > 5 && date.getHours() < 18) theme.classList.add("day");
         else theme.classList.add("night");
 
         day.innerText = dayArr[date.getDay()] + ",";
         date.innerText = date.toString().substring(4, 10);
-        place.innerText = data.location.name;
-        condition.innerText = data.current.condition.text;
-        temperature.innerText = data.current.temp_c;
+        place.innerText = data.name;
+        condition.innerText = data.condition;
+        temperature.innerText = data.temp_c;
         image.setAttribute("src", "./image/location.png");
 
     } catch (e) {
